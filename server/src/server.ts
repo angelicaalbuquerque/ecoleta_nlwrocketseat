6bdcc26de2099214d2bb3f62cb26970b1b0bd20e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-01 23:18:43 -0300

    Instalação de dependências e criação de rota users

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,11 @@
 import express from "express";
 
 const app = express();
+
+app.get("/users", (request, response) => {
+  console.log("listagem de usuários");
+
+  response.json(["Angélica", "Emidio", "Flávio", "Larissa"]);
+});
+
+app.listen(3333);
