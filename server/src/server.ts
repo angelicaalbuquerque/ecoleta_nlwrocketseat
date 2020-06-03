7e253a2f2c5bf681e9908b0ee64e81a43d7aa385
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-03 20:22:27 -0300

    Busca de usuário único

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,20 @@ import express from "express";
 
 const app = express();
 
+const users = ["Angélica", "Emidio", "Flávio", "Larissa"];
+
 app.get("/users", (request, response) => {
   console.log("listagem de usuários");
 
-  return response.json(["Angélica", "Emidio", "Flávio", "Larissa"]);
+  return response.json(users);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
 });
 
 app.post("/users", (request, response) => {
