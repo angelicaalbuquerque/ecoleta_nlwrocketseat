9fe6b410d034b6afb741c713484bf9ff39ba832e
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-02 22:58:05 -0300

    Simulação de criação de usuário

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,15 @@ const app = express();
 app.get("/users", (request, response) => {
   console.log("listagem de usuários");
 
-  response.json(["Angélica", "Emidio", "Flávio", "Larissa"]);
+  return response.json(["Angélica", "Emidio", "Flávio", "Larissa"]);
+});
+
+app.post("/users", (request, response) => {
+  const user = {
+    name: "Angélica",
+    email: "angelica@provedor.com",
+  };
+  return response.json(user);
 });
 
 app.listen(3333);
