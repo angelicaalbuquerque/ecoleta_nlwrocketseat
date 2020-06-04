65a1feb74b3accb7b582f9b67d1bdeaaf202291a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-03 21:56:09 -0300

    Remoção de rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,39 +2,10 @@ import express from "express";
 
 const app = express();
 
-//para que o express entenda JSON
 app.use(express.json());
 
-const users = ["Angélica", "Emidio", "Flávio", "Larissa"];
-
-app.get("/users", (request, response) => {
-  const search = String(request.query.search);
-
-  const filteredUsers = search
-    ? users.filter((user) => user.includes(search))
-    : users;
-
-  return response.json(filteredUsers);
-});
-
-app.get("/users/:id", (request, response) => {
-  const id = Number(request.params.id);
-
-  const user = users[id];
-
-  return response.json(user);
-});
-
-app.post("/users", (request, response) => {
-  const data = request.body;
-
-  console.log(data);
-
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-  return response.json(user);
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
 });
 
 app.listen(3333);
