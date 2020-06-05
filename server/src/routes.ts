605a789c51c6dea5f0baa21e86083f9284229df3
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-04 22:52:51 -0300

    transaction

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -29,7 +29,9 @@ routes.post("/points", async (request, response) => {
     items,
   } = request.body;
 
-  const ids = await knex("points").insert({
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("points").insert({
     image: "image-fake",
     name,
     email,
@@ -40,14 +42,16 @@ routes.post("/points", async (request, response) => {
     uf,
   });
 
+  const point_id = insertedIds[0];
+
   const pointItems = items.map((item_id: number) => {
     return {
       item_id,
-      point_id: ids[0],
+      point_id,
     };
   });
 
-  await knex("point_items").insert(pointItems);
+  await trx("point_items").insert(pointItems);
 
   return response.json({ success: true });
 });
