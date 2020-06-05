81710059c353d64c4e8ab0f0029dea1bc704aa82
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-04 22:32:56 -0300

    Rota de criação de ponto de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,39 @@ routes.get("/items", async (request, response) => {
 
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
   });
 
   return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  await knex("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  return response.json({ success: true });
 });
 
 export default routes;
