225add1c67e8f744f7e1f133d071d8558f19cbaa
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-04 21:46:58 -0300

    Upload de imagens e acesso aos dados da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import routes from "./routes";
 
 const app = express();
@@ -6,5 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
