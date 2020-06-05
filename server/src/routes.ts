89a9414c0ef050b1f06e523a31edfc620cee67fc
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-04 22:42:41 -0300

    Relacionamento do ponto com a tabela de itens

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -29,7 +29,7 @@ routes.post("/points", async (request, response) => {
     items,
   } = request.body;
 
-  await knex("points").insert({
+  const ids = await knex("points").insert({
     image: "image-fake",
     name,
     email,
@@ -39,6 +39,15 @@ routes.post("/points", async (request, response) => {
     city,
     uf,
   });
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    };
+  });
+
+  await knex("point_items").insert(pointItems);
 
   return response.json({ success: true });
 });
