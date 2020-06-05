e06143d198128a5dc1ea484b44dc6c9f9bcecaab
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-04 23:34:52 -0300

    Listagem de ponto específico

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -2,6 +2,28 @@ import { Request, Response } from "express";
 import knex from "../database/connection";
 
 class PointsController {
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const point = await knex("points").where("id", id).first();
+
+    if (!point) {
+      return response.status(400).json({ message: "Point not found." });
+    }
+
+    /*
+     *  SELECT * FROM items
+     *     JOIN point_items ON items.id = point_items.item_id
+     *     WHERE point_items.point_id = {id}
+     */
+    const items = await knex("items")
+      .join("point_items", "items.id", "=", "point_items.item_id")
+      .where("point_items.point_id", id)
+      .select("items.title");
+
+    return response.json({ point, items });
+  }
+
   async create(request: Request, response: Response) {
     const {
       name,
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -9,5 +9,6 @@ const itemsController = new ItemsController();
 routes.get("/items", itemsController.index);
 
 routes.post("/points", pointsController.create);
+routes.get("/points/:id", pointsController.show);
 
 export default routes;
