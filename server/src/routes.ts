b1d698f211e05923c0478b5ffdeb4dfd73c7289a
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-04 23:15:39 -0300

    Desacoplamento de código/criação de patterns

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,13 @@
 import express from "express";
-import knex from "./database/connection";
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/itemsController";
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get("/items", async (request, response) => {
-  const items = await knex("items").select("*");
+routes.get("/items", itemsController.index);
 
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-
-  return response.json(serializedItems);
-});
-
-routes.post("/points", async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = request.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx("points").insert({
-    image: "image-fake",
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id,
-      point_id,
-    };
-  });
-
-  await trx("point_items").insert(pointItems);
-
-  return response.json({ success: true });
-});
+routes.post("/points", pointsController.create);
 
 export default routes;
