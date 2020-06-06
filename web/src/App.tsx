401dfa1441243a2c8dc885a70b7a47b28d69bfdd
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-06 12:39:33 -0300

    Limpeza de estrutura

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, World!</h1>
-      </header>
-    </div>
-  );
+  return <h1>Hello, World!</h1>;
 }
 
 export default App;
