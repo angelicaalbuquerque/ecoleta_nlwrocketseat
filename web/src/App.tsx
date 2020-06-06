e9714a7859d0550d36d67e15ab99a91b5cec499d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-06 16:27:52 -0300

    Limpeza de código para início da construção

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Header from "./Header";
 
 function App() {
-  const [counter, setCounter] = useState(0); // [valor do estado, função para atualizar o valor do estado]
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <div>
-      <Header title={`Contador: ${counter}`} />
-
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>
-        Aumentar
-      </button>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
