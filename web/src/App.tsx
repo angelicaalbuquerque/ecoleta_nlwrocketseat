4835dc397c55f1f2b7cf6b60c1e219bd00ea2382
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-06 15:05:32 -0300

    Criação do componente Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <Header />
+
+      <h1>Conteúdo da aplicação</h1>
+    </div>
+  );
 }
 
 export default App;
