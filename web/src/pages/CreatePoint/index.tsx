7d7a6459041a67f1c3de6d26a3542fee4e0992ca
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-07 12:54:41 -0300

    Buscando estados e cidades do IBGE

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -26,6 +26,7 @@ const CreatePoint = () => {
   const [cities, setCities] = useState<string[]>([]);
   const [ufs, setUfs] = useState<string[]>([]);
   const [selectedUf, setSelectedUf] = useState("0");
+  const [selectedCity, setSelectedCity] = useState("0");
 
   useEffect(() => {
     api.get("items").then((response) => {
@@ -62,6 +63,11 @@ const CreatePoint = () => {
   function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value;
     setSelectedUf(uf);
+  }
+
+  function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+    setSelectedUf(city);
   }
 
   return (
@@ -138,7 +144,12 @@ const CreatePoint = () => {
 
             <div className="field">
               <label htmlFor="city">Cidade</label>
-              <select name="city" id="city">
+              <select
+                name="city"
+                id="city"
+                value={selectedCity}
+                onChange={handleSelectedCity}
+              >
                 <option value="0">Selecione uma cidade</option>
                 {cities.map((city) => (
                   <option key={city} value={city}>
