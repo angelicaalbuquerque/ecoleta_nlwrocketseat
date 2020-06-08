8c51a59fe4ae3f2b52e393cbf1168d6e3f41de59
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-08 18:13:16 -0300

    Criação de novas páginas e rotas

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "./pages/Home";
+import Points from "./pages/Points";
+import Detail from "./pages/Detail";
+
+const AppStack = createStackNavigator();
