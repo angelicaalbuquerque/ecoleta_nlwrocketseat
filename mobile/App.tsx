c5c6cf755d304e96d147f7ec1893e543749fd36d
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-07 22:13:14 -0300

    Configuração da StatusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, View } from "react-native";
 
 import Home from "./src/pages/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
 }
