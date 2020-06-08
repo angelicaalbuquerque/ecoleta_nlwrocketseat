8f3e83f0d27983dace9b6f873be9a5ad97c3f428
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-07 23:02:13 -0300

    Finalização da página Home mobile

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Feather as Icon } from "@expo/vector-icons";
 import { View, ImageBackground, Text, Image, StyleSheet } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 
 const Home = () => {
   return (
@@ -12,8 +14,19 @@ const Home = () => {
         <Image source={require("../../assets/logo.png")} />
         <Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
         <Text style={styles.description}>
-          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
         </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
       </View>
     </ImageBackground>
   );
