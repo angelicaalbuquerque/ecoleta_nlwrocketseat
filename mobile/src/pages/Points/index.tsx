84ccd079c40102fb7e42007b4db16bfc48bca862
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-09 00:29:11 -0300

    Finalização da aplicação mobile

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Constants from "expo-constants";
 import { Feather as Icon } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import {
   View,
   Text,
@@ -31,8 +31,17 @@ interface Point {
   longitude: number;
 }
 
+interface Params {
+  uf: string;
+  city: string;
+}
+
 const Points = () => {
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
   const [initialPosition, setInitialPosition] = useState<[number, number]>([
     0,
     0,
@@ -74,15 +83,15 @@ const Points = () => {
     api
       .get("points", {
         params: {
-          city: "Rio de Janeiro",
-          uf: "RJ",
-          items: [1, 2, 6],
+          city: routeParams.city,
+          uf: routeParams.uf,
+          items: selectedItems,
         },
       })
       .then((response) => {
         setPoints(response.data);
       });
-  }, []);
+  }, [selectedItems]);
 
   function handleNavigateBack() {
     navigation.goBack();
