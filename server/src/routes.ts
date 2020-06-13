46fe3f1bbbcfc59b4e49644673127268bd7c9244
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2020-06-12 23:23:45 -0300

    Validação de entrada de dados no backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import multerConfig from "./config/multer";
+import { celebrate, Joi } from "celebrate";
 
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
@@ -16,6 +17,27 @@ routes.get("/items", itemsController.index);
 routes.get("/points", pointsController.index);
 routes.get("/points/:id", pointsController.show);
 
-routes.post("/points", upload.single("image"), pointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    },
+  ),
+  pointsController.create,
+);
 
 export default routes;
